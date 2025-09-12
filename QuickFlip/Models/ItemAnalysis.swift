b875d5a4c449b47ab23999bd7e80a1a406ef6e06
Coury Ferson <coury.ferson@disney.com>
2025-09-11 19:15:55 -0500

    Move ItemAnalysis to its own model class

---
+++ QuickFlip/Models/ItemAnalysis.swift
@@ -0,0 +1,15 @@
+//
+//  ItemAnalysis.swift
+//  QuickFlip
+//
+//  Created by Ferson, Coury on 9/11/25.
+//
+
+// MARK: - Item Analysis Model
+struct ItemAnalysis: Equatable {
+    let itemName: String
+    let condition: String
+    let description: String
+    let estimatedValue: String
+    let category: String
+}
